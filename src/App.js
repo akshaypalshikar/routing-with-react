fe4c2196d6ba11da9a82e282db801dcffa76c2eb
akshaypalshikar <aksmakesit@gmail.com>
2018-08-19 16:23:47 -0700

    routing start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Blog />
+      </div>
     );
   }
 }
